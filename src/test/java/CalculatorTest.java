6804d942d26d3c54516a51aa2dc1eb73c0c37d57
Jordan Wang <jordanwang@Objectivasoftware.com>
2019-03-22 15:29:45 +0800

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+
+    public void testAdd() {
+        Calculator c = new Calculator();
+        int result = c.add(1, 2);
+        assertEquals(result, 3);
+    }
+
+    @Test
+
+    public void testSub() {
+        Calculator c = new Calculator();
+        int result = c.sub(2, 1);
+        assertEquals(result, 1);
+    }
+
+}
